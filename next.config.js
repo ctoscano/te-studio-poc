97809870ff9e6df1b4470c3c309fabf5e1965848
Cesar Toscano <hello@ctoscano.com>
2022-04-21 15:43:32 -0700

    fixed base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { images: { layoutRaw: true } },
-  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+  basePath: process.env.NODE_ENV == "production" ? "/te-web" : "/",
 }
